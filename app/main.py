df81c551bc065aa1b8c75c87b571d315199ce6b7
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-14 14:05:57 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/hello/{name}")
-def read_name(name:,str = None):
+def read_name(name: str = None):
     return {"hello":name}
 
 
